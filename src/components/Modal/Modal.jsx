a02087051fc2845671cd76a77430ca1467b4f4ff
Andriy Yarmosh <101985044+HeadOd@users.noreply.github.com>
2022-12-16 17:34:18 +0100

    remastered dependens

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,26 +4,28 @@ import { createPortal } from "react-dom";
 const modalRoot = document.querySelector('#modal-root');
 
 export const Modal = ({ bigImg, onClose }) => {
-  function handleKeyDown(e) {
-    if (e.code === 'Escape') {
-      onClose();
-    }
-
-    if(e.currentTarget === e.target) {
-      onClose();
-    }
-  }
-
   useEffect(() => {
+    function handleKeyDown(e) {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    }
+    
     window.addEventListener('keydown', handleKeyDown);
 
     return () => {
       window.removeEventListener('keydown', handleKeyDown)
     };
-  }, [handleKeyDown]);
+  }, []);
+
+  function handleTarget(e) {
+    if(e.currentTarget === e.target) {
+      onClose();
+    }
+  }
 
   return createPortal(
-    <div className="overlay" onClick={handleKeyDown}>
+    <div className="overlay" onClick={handleTarget}>
       <div className="modal">
         <img src={bigImg} alt="" />
       </div>
